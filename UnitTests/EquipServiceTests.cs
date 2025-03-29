e605df5021f100cdb2fc33aa215f863d222a9e7d
DESKTOP-QMTP0ML\1 <ulyanovaleksei@gmail.com>
2025-03-29 21:34:21 +0300

    #4 Создан юнит тест для бизнес сервиса

---
+++ UnitTests/EquipServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Application.Equips.Dto;
+using Application.Equips.Services;
+using Data;
+using Moq;
+
+namespace UnitTests
+{
+    public class EquipServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void BusinessExceptionTest()
+        {
+            // Arrange
+            var mock = new Mock<IDataLayer>();
+
+
+            IEquipService equipService = new EquipService(mock.Object);
+            EquipDto equipDto = new();
+
+
+            // Assert
+            Assert.Throws<ApplicationException>(
+                    // Action
+                    () => equipService.Create(equipDto)
+                );
+        }
+    }
+}
